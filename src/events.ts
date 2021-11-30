b2c1ea19745725243bf58671b2e59961a78da0a2
Luka-sama <luka-sama@pm.me>
2021-11-30 01:11:15 +0100

    Add class transformer and class validator

--- src/events.ts
+++
@@ -1,7 +0,0 @@
-import {Events, Socket} from "./ws";
-
-/** Object with router events */
-const events: Events = {
-	"say hello": (sck: Socket, b: string) => sck.emit("kwa", "hallo " + b)
-};
-export default events;
