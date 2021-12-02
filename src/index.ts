d518fc5a616578399a1340fd947f69c807c5863d
Luka-sama <luka-sama@pm.me>
2021-12-02 23:54:09 +0100

    Auto-import of all modules; index.js handles uncaught exceptions and unhandled rejections

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
+import glob from "glob";
+import * as path from "path";
 import "reflect-metadata";
-import "./auth";
 import WS from "./ws";
 
-WS.init();
+const ignore = ["./dist/entities/**", "./dist/**/*.test.js"];
+const fileList = glob.sync("./dist/**/*.js", {ignore});
+// Auto-import to make @OnlyLogged() and other descriptors to work without explicit import
+for (const file of fileList) {
+	import(path.resolve(file));
+}
+
+(async function() {
+	process.on("uncaughtException", error => {
+		console.error(`UNCAUGHT EXCEPTION [${Date.now()}]:\r\n${error.stack}`);
+	});
+
+	process.on("unhandledRejection", error => {
+		console.error(`UNHANDLED REJECTION [${Date.now()}]:\r\n${error}`);
+	});
+
+	await WS.init();
+})();
