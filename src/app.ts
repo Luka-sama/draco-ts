f15452d34572d4501377e916250765f577a83f83
Luka-sama <luka-sama@pm.me>
2022-01-18 00:03:22 +0100

    Move initialization to new class App

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import glob from "glob";
+import path from "path";
+import ORM from "./orm";
+import WS from "./ws";
+
+export default class App {
+	private static started = false;
+
+	/** Auto-import to make @OnlyLogged() and other descriptors to work without explicit import */
+	static async init() {
+		if (App.started) {
+			return;
+		}
+		App.started = true;
+
+		App.autoimport();
+		App.catchExceptions();
+		await ORM.init();
+		await WS.init();
+	}
+
+	private static autoimport() {
+		const ignore = ["./dist/**/*.entity.js", "./dist/**/*.test.js"];
+		const fileList = glob.sync("./dist/**/*.js", {ignore});
+		for (const file of fileList) {
+			import(path.resolve(file));
+		}
+	}
+
+	private static catchExceptions() {
+		process.on("uncaughtException", error => {
+			console.error(`UNCAUGHT EXCEPTION [${Date.now()}]:\r\n${error.stack}`);
+		});
+
+		process.on("unhandledRejection", error => {
+			console.error(`UNHANDLED REJECTION [${Date.now()}]:\r\n${error}`);
+		});
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,4 @@
-import glob from "glob";
-import * as path from "path";
 import "reflect-metadata";
-import WS from "./ws";
+import App from "./app";
 
-const ignore = ["./dist/entities/**", "./dist/**/*.test.js"];
-const fileList = glob.sync("./dist/**/*.js", {ignore});
-// Auto-import to make @OnlyLogged() and other descriptors to work without explicit import
-for (const file of fileList) {
-	import(path.resolve(file));
-}
-
-(async function() {
-	process.on("uncaughtException", error => {
-		console.error(`UNCAUGHT EXCEPTION [${Date.now()}]:\r\n${error.stack}`);
-	});
-
-	process.on("unhandledRejection", error => {
-		console.error(`UNHANDLED REJECTION [${Date.now()}]:\r\n${error}`);
-	});
-
-	await WS.init();
-})();
+App.init();
