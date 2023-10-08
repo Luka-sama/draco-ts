33ae3bc2675710924f7ea3d770f095631081c915
Luka-sama <luka-sama@pm.me>
2023-10-08 21:16:37 +0200

    Refactor game loop

---
+++ src/draco-ts/game-loop.test.ts
@@ -0,0 +1,129 @@
+import GameLoop from "./game-loop.js";
+
+const frequency = GameLoop["FREQUENCY"];
+const logError = jest.spyOn(GameLoop["logger"], "error").mockImplementation();
+beforeAll(() => {
+	jest.useFakeTimers();
+	GameLoop.init();
+});
+
+test("single task with limited execution count", () => {
+	let counter = 0;
+	GameLoop.addTask(() => {
+		counter++;
+	}, 0, 2);
+
+	jest.advanceTimersByTime(frequency);
+	expect(counter).toBe(1);
+	jest.advanceTimersByTime(frequency);
+	expect(counter).toBe(2);
+	jest.advanceTimersByTime(5 * frequency);
+	expect(counter).toBe(2);
+});
+
+test("slow and fast task in parallel", async () => {
+	const taskDuration = 1000;
+	let slowTaskCounter = 0, fastTaskCounter = 0, slowTaskInstanceCount = 0;
+	const slowTask = GameLoop.addTask(async () => {
+		slowTaskInstanceCount++;
+		return new Promise(resolve => {
+			setTimeout(() => {
+				slowTaskCounter++;
+				slowTaskInstanceCount--;
+				resolve();
+			}, taskDuration);
+		});
+	});
+	const fastTask = GameLoop.addTask(() => {
+		fastTaskCounter++;
+	});
+
+	await jest.advanceTimersByTimeAsync(2 * frequency);
+	expect(slowTaskCounter).toBe(0);
+	expect(slowTaskInstanceCount).toBe(1);
+	expect(fastTaskCounter).toBe(2);
+
+	await jest.advanceTimersByTimeAsync(taskDuration);
+	expect(slowTaskCounter).toBe(1);
+	expect(slowTaskInstanceCount).toBeLessThanOrEqual(1);
+	expect(fastTaskCounter).toBe(Math.floor(2 + taskDuration / frequency));
+
+	await jest.advanceTimersByTimeAsync(taskDuration);
+	expect(slowTaskCounter).toBe(2);
+	expect(slowTaskInstanceCount).toBeLessThanOrEqual(1);
+	expect(fastTaskCounter).toBe(Math.floor(2 + 2 * taskDuration / frequency));
+
+	GameLoop.removeTask(slowTask);
+	GameLoop.removeTask(fastTask);
+	await jest.advanceTimersByTimeAsync(3 * taskDuration);
+	expect(slowTaskCounter).toBe(3);
+	expect(slowTaskInstanceCount).toBe(0);
+	expect(fastTaskCounter).toBe(Math.floor(2 + 2 * taskDuration / frequency));
+});
+
+test("task with error", () => {
+	let taskWithErrorCounter = 0;
+	let taskWithoutErrorCounter = 0;
+	const taskWithError = GameLoop.addTask(() => {
+		taskWithErrorCounter++;
+		throw new Error("some error");
+	});
+	const taskWithoutError = GameLoop.addTask(() => {
+		taskWithoutErrorCounter++;
+	});
+
+	jest.advanceTimersByTime(3 * frequency);
+	expect(taskWithErrorCounter).toBe(3);
+	expect(taskWithoutErrorCounter).toBe(3);
+	expect(logError).toHaveBeenCalledTimes(3);
+
+	GameLoop.removeTask(taskWithError);
+	GameLoop.removeTask(taskWithoutError);
+});
+
+test("async task with error", async () => {
+	let taskWithErrorCounter = 0;
+	let taskWithoutErrorCounter = 0;
+	const taskWithError = GameLoop.addTask(() => {
+		taskWithErrorCounter++;
+		return new Promise((resolve, reject) => {
+			reject("some error");
+		});
+	});
+	const taskWithoutError = GameLoop.addTask(() => {
+		taskWithoutErrorCounter++;
+	});
+
+	await jest.advanceTimersByTimeAsync(4 * frequency);
+	expect(taskWithErrorCounter).toBe(4);
+	expect(taskWithoutErrorCounter).toBe(4);
+	expect(logError).toHaveBeenCalledTimes(4);
+
+	GameLoop.removeTask(taskWithError);
+	GameLoop.removeTask(taskWithoutError);
+});
+
+test("using delta", async () => {
+	let fastTaskDeltaSum = 0, slowTaskDeltaSum = 0;
+	const slowTaskDuration = 100;
+	const fastTask = GameLoop.addTask(delta => {
+		fastTaskDeltaSum += delta;
+	});
+	const slowTask = GameLoop.addTask(delta => {
+		slowTaskDeltaSum += delta;
+		return new Promise(resolve => setTimeout(resolve, slowTaskDuration));
+	});
+
+	await jest.advanceTimersByTimeAsync(4 * frequency + 2);
+	expect(fastTaskDeltaSum).toBe(4 * frequency);
+	expect(slowTaskDeltaSum).toBe(frequency);
+	GameLoop.removeTask(fastTask);
+
+	await jest.advanceTimersByTimeAsync(slowTaskDuration);
+	expect(slowTaskDeltaSum).toBe(frequency + Math.ceil(slowTaskDuration / frequency) * frequency);
+
+	await jest.advanceTimersByTimeAsync(slowTaskDuration);
+	expect(slowTaskDeltaSum).toBe(frequency + 2 * Math.ceil(slowTaskDuration / frequency) * frequency);
+
+	GameLoop.removeTask(slowTask);
+});
--- src/draco-ts/game-loop.ts
+++ src/draco-ts/game-loop.ts
@@ -1,53 +1,98 @@
 import _ from "lodash";
-import {EndOfRequest} from "./limit.js";
+import Logger from "./logger.js";
 
+/** A function that can be a task in game loop */
 export type TaskFunction = (delta: number) => void | Promise<void>;
+/** An object that contains the information about the task */
 export interface Task {
-	task: TaskFunction,
+	taskFunction: TaskFunction,
 	frequency: number,
 	lastExecution: number,
+	remainingExecutions?: number,
 }
 
+/**
+ * Game loop class.
+ * It can be used to add tasks that should be repeated periodically or that should be delayed and executed limited number of times.
+ */
 export default class GameLoop {
 	/** How often the game loop is executed */
 	private static readonly FREQUENCY = 16;
-	private static tasks: Task[] = [];
+	private static readonly logger = new Logger(GameLoop);
+	private static readonly tasks: Task[] = [];
 
 	/** Initializes game loop */
-	static init() {
-		setInterval(GameLoop.exec, GameLoop.FREQUENCY).unref();
+	public static init(): void {
+		setInterval(GameLoop.execAllTasks, GameLoop.FREQUENCY).unref();
 	}
 
-	/** Adds task to the list of executed tasks. If `frequency` is not specified, the task is executed in each game loop iteration */
-	static addTask(taskFunction: TaskFunction, frequency = 0): Task {
-		const task: Task = {task: taskFunction, frequency, lastExecution: Date.now()};
+	/**
+	 * Adds task to the list of executed tasks.
+	 * If `frequency` is not specified, the task is executed in each game loop iteration.
+	 * If `executionCount` is not specified, the task is executed infinitely. With `executionCount` 1 the task is a timeout.
+	 */
+	public static addTask(taskFunction: TaskFunction, frequency = 0, executionCount?: number): Task {
+		const task: Task = {taskFunction, frequency, lastExecution: Date.now(), remainingExecutions: executionCount};
 		GameLoop.tasks.push(task);
+		const name = taskFunction.name || "An anonymous function";
+		GameLoop.logger.info(
+			`${name} was added to tasks with frequency ${frequency}` +
+			(executionCount !== undefined ? ` and execution count ${executionCount}.` : ".")
+		);
 		return task;
 	}
 
-	static removeTask(task: Task): void {
-		_.pull(GameLoop.tasks, task);
+	/** Shortcut for {@link GameLoop.addTask} with `executionCount` 1 */
+	public static addTimeout(taskFunction: TaskFunction, ms: number): Task {
+		return GameLoop.addTask(taskFunction, ms, 1);
 	}
 
-	private static async exec(): Promise<void> {
-		const now = Date.now();
-		const promises = [];
+	/** Removes task from the list of executed tasks */
+	public static removeTask(task: Task): void {
+		_.pull(GameLoop.tasks, task);
+		GameLoop.logger.info(`${task.taskFunction.name || "An anonymous function"} was removed from tasks.`);
+	}
 
-		for (const task of GameLoop.tasks) {
-			const delta = now - task.lastExecution;
-			if (delta >= task.frequency) {
-				task.lastExecution = Infinity; // Task locking, prevents the simultaneous execution of two identical tasks
-				const promise = Promise.resolve(task.task(delta)).catch(e => {
-					if (!(e instanceof EndOfRequest) && (e as any)?.code != "ABORT_ERR") {
-						console.error(e);
-					}
-				}).finally(() => {
-					task.lastExecution = now;
-				});
-				promises.push(promise);
-			}
+	/** Executes all tasks during a loop iteration */
+	private static async execAllTasks(): Promise<void> {
+		await Promise.allSettled(
+			GameLoop.tasks.map(GameLoop.execTask)
+		);
+	}
+
+	/** Executes a task during a loop iteration */
+	private static async execTask(task: Task): Promise<void> {
+		const now = Date.now();
+		const delta = now - task.lastExecution;
+		if (delta < task.frequency) {
+			return;
 		}
 
-		await Promise.allSettled(promises);
+		task.lastExecution = Infinity; // Task locking, prevents the simultaneous execution of two identical tasks
+		GameLoop.logger.debug(`${task.taskFunction.name || "An anonymous function"} starts executing.`);
+		try {
+			const result = task.taskFunction(delta);
+			if (result) {
+				return result.catch(GameLoop.logger.error).finally(() => GameLoop.onTaskEnd(task, now));
+			} else {
+				GameLoop.onTaskEnd(task, now);
+			}
+		} catch(e) {
+			GameLoop.logger.error(e);
+			GameLoop.onTaskEnd(task, now);
+		}
+	}
+
+	/** The function that is executed at the end of each task */
+	private static onTaskEnd(task: Task, now: number): void {
+		task.lastExecution = now;
+		GameLoop.logger.debug(`${task.taskFunction.name || "An anonymous function"} ends executing.`);
+
+		if (task.remainingExecutions !== undefined) {
+			task.remainingExecutions--;
+			if (task.remainingExecutions < 1) {
+				GameLoop.removeTask(task);
+			}
+		}
 	}
 }
