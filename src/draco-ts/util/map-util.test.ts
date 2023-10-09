85b176c6224102c1b4440812327b3f4594fc6963
Luka-sama <luka-sama@pm.me>
2023-10-09 15:46:52 +0200

    Refactor MapUtil

---
+++ src/draco-ts/util/map-util.test.ts
@@ -0,0 +1,50 @@
+import MapUtil from "./map-util.js";
+
+const object = {"nestedKey": 123};
+
+test("get", () => {
+	const map = new Map<string, number>;
+	expect(MapUtil.get(map, "key", 123)).toBe(123);
+	expect(MapUtil.get(map, "key", 100)).toBe(123);
+});
+
+test("getArray", () => {
+	const map = new Map<string, number[]>;
+	const array = MapUtil.getArray(map, "key");
+	expect(array).toEqual([]);
+	array.push(123);
+	expect(MapUtil.getArray(map, "key")).toEqual([123]);
+});
+
+test("getMap", () => {
+	const map = new Map<string, Map<string, number>>;
+	const nestedMap = MapUtil.getMap(map, "key");
+	expect(nestedMap).toEqual(new Map);
+	nestedMap.set("nestedKey", 123);
+	expect(MapUtil.getMap(map, "key")).toEqual(new Map([["nestedKey", 123]]));
+});
+
+test("getSet", () => {
+	const map = new Map<string, Set<number>>;
+	const set = MapUtil.getSet(map, "key");
+	expect(set).toEqual(new Set);
+	set.add(123);
+	expect(MapUtil.getSet(map, "key")).toEqual(new Set([123]));
+});
+
+test("getWeakMap", () => {
+	const map = new Map<string, WeakMap<Object, number>>;
+	const weakMap = MapUtil.getWeakMap(map, "key");
+	expect(weakMap).toEqual(new WeakMap);
+	weakMap.set(object, 123);
+	expect(MapUtil.getWeakMap(map, "key").get(object)).toBe(123);
+});
+
+test("getWeakSet", () => {
+	const map = new Map<string, WeakSet<Object>>;
+	const set = MapUtil.getWeakSet(map, "key");
+	expect(set).toEqual(new WeakSet);
+	expect(set.has(object)).toBeFalsy();
+	set.add(object);
+	expect(MapUtil.getWeakSet(map, "key").has(object)).toBeTruthy();
+});
--- src/draco-ts/util/map-util.ts
+++ src/draco-ts/util/map-util.ts
@@ -1,5 +1,7 @@
+/** Helper class that provides different operations with maps */
 export default class MapUtil {
-	static get<K, V>(map: Map<K, V>, key: K, defaultValue: V): V {
+	/** Returns an element of `map` with the given key. If this element doesn't exist, it will first create it with value `defaultValue` */
+	public static get<K, V>(map: Map<K, V>, key: K, defaultValue: V): V {
 		const value = map.get(key);
 		if (value === undefined) {
 			map.set(key, defaultValue);
@@ -8,19 +10,28 @@ export default class MapUtil {
 		return value;
 	}
 
-	static getArray<K, V extends any[]>(map: Map<K, V>, key: K): V {
-		return MapUtil.get(map, key, [] as unknown as V);
+	/** Shortcut for `MapUtil.get(map, key, [])`. See {@link MapUtil.get} for details */
+	public static getArray<K, V extends Array<unknown>>(map: Map<K, V>, key: K): V {
+		return MapUtil.get(map, key, new Array as V);
 	}
 
-	static getMap<K, V extends Map<any, any>>(map: Map<K, V>, key: K): V {
+	/** Shortcut for `MapUtil.get(map, key, new Map)`. See {@link MapUtil.get} for details */
+	public static getMap<K, V extends Map<unknown, unknown>>(map: Map<K, V>, key: K): V {
 		return MapUtil.get(map, key, new Map as V);
 	}
 
-	static getSet<K, V extends Set<any>>(map: Map<K, V>, key: K): V {
+	/** Shortcut for `MapUtil.get(map, key, new Set)`. See {@link MapUtil.get} for details */
+	public static getSet<K, T, V extends Set<unknown>>(map: Map<K, V>, key: K): V {
 		return MapUtil.get(map, key, new Set as V);
 	}
 
-	static getWeakMap<K, V extends WeakMap<any, any>>(map: Map<K, V>, key: K): V {
+	/** Shortcut for `MapUtil.get(map, key, new WeakMap)`. See {@link MapUtil.get} for details */
+	public static getWeakMap<K, V extends WeakMap<Object, unknown>>(map: Map<K, V>, key: K): V {
 		return MapUtil.get(map, key, new WeakMap as V);
+	}
+
+	/** Shortcut for `MapUtil.get(map, key, new WeakSet)`. See {@link MapUtil.get} for details */
+	public static getWeakSet<K, V extends WeakSet<Object>>(map: Map<K, V>, key: K): V {
+		return MapUtil.get(map, key, new WeakSet as V);
 	}
 }
