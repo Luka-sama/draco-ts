0cf96da311b1f1ebb60820fc8c28056b0fc77e95
Luka-sama <luka-sama@pm.me>
2023-10-09 15:11:13 +0200

    Refactor SetUtil

---
+++ src/draco-ts/util/set-util.test.ts
@@ -0,0 +1,50 @@
+import SetUtil from "./set-util.js";
+
+const A = new Set([1, 2, 3, 4]);
+const B = new Set([2, 4, 6, 8]);
+
+test("merge", () => {
+	const C = new Set([2, 10]);
+	SetUtil.merge(C, A);
+	expect(C).toEqual(new Set([2, 10, 1, 3, 4]));
+});
+
+test("isSuperset", () => {
+	const C = new Set([0, 1, 2, 3, 4, 5, 6, 7]);
+	expect(SetUtil.isSuperset(A, B)).toBeFalsy();
+	expect(SetUtil.isSuperset(C, A)).toBeTruthy();
+});
+
+test("isSubset", () => {
+	const C = new Set([1, 4]);
+	expect(SetUtil.isSubset(A, B)).toBeFalsy();
+	expect(SetUtil.isSubset(C, A)).toBeTruthy();
+});
+
+test("haveCommonElements", () => {
+	const C = new Set([10, 20]);
+	expect(SetUtil.haveCommonElements(A, B)).toBeTruthy();
+	expect(SetUtil.haveCommonElements(A, C)).toBeFalsy();
+});
+
+test("areEqual", () => {
+	const C = new Set([1, 2, 3, 4]);
+	expect(SetUtil.areEqual(A, B)).toBeFalsy();
+	expect(SetUtil.areEqual(A, C)).toBeTruthy();
+});
+
+test("union", () => {
+	expect(SetUtil.union(A, B)).toEqual(new Set([1, 2, 3, 4, 6, 8]));
+});
+
+test("intersection", () => {
+	expect(SetUtil.intersection(A, B)).toEqual(new Set([2, 4]));
+});
+
+test("difference", () => {
+	expect(SetUtil.difference(A, B)).toEqual(new Set([1, 3]));
+});
+
+test("symmetricDifference", () => {
+	expect(SetUtil.symmetricDifference(A, B)).toEqual(new Set([1, 3, 6, 8]));
+});
--- src/draco-ts/util/set-util.ts
+++ src/draco-ts/util/set-util.ts
@@ -1,26 +1,73 @@
+/** Helper class that provides different operations with sets */
 export default class SetUtil {
 	/** Merges B into A */
-	static merge<T>(A: Set<T>, B: Set<T>): void {
+	public static merge<T>(A: Set<T>, B: Set<T>): void {
 		B.forEach(A.add, A);
 	}
 
-	/** Returns set with elements that are both in A and in B */
-	static intersection<T>(A: Set<T>, B: Set<T>): Set<T> {
-		const result = new Set<T>;
+	/** Checks if A is superset of B, i.e. all elements of B are also elements of A */
+	public static isSuperset<T>(A: Set<T>, B: Set<T>): boolean {
+		for (const value of B) {
+			if (!A.has(value)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/** Checks if A is subset of B, i.e. all elements of A are also elements of B */
+	public static isSubset<T>(A: Set<T>, B: Set<T>): boolean {
+		return SetUtil.isSuperset(B, A);
+	}
+
+	/** Checks if A is superset of B, i.e. all elements of B are also elements of A */
+	public static haveCommonElements<T>(A: Set<T>, B: Set<T>): boolean {
 		for (const value of A) {
 			if (B.has(value)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/** Checks if A and B are equal, i.e. have the same elements */
+	public static areEqual<T>(A: Set<T>, B: Set<T>): boolean {
+		return SetUtil.isSuperset(A, B) && A.size == B.size;
+	}
+
+	/** Returns a set that contains the elements of both A and B */
+	public static union<T>(A: Set<T>, B: Set<T>): Set<T> {
+		return SetUtil.op(A, B, (hasA, hasB) => hasA || hasB);
+	}
+
+	/** Returns a set that contains those elements of A that are also in B */
+	public static intersection<T>(A: Set<T>, B: Set<T>): Set<T> {
+		return SetUtil.op(A, B, (hasA, hasB) => hasA && hasB);
+	}
+
+	/** Returns a set that contains those elements of A that are not in B */
+	public static difference<T>(A: Set<T>, B: Set<T>): Set<T> {
+		return SetUtil.op(A, B, (hasA, hasB) => hasA && !hasB);
+	}
+
+	/** Returns a set of all elements which are in A or B but not both */
+	public static symmetricDifference<T>(A: Set<T>, B: Set<T>): Set<T> {
+		return SetUtil.op(A, B, (hasA, hasB) => (hasA && !hasB) || (!hasA && hasB));
+	}
+
+	/** Returns a set that contains those elements of A and B for which `f` returns true */
+	private static op<T>(A: Set<T>, B: Set<T>, f: (hasA: boolean, hasB: boolean) => boolean): Set<T> {
+		const result = new Set<T>;
+		for (const value of A) {
+			if (f(true, B.has(value))) {
 				result.add(value);
 			}
 		}
-		return result;
-	}
-
-	/** Returns set with elements that are in A, but not in B */
-	static difference<T>(A: Set<T>, B: Set<T>): Set<T> {
-		const result = new Set<T>;
-		for (const value of A) {
-			if (!B.has(value)) {
-				result.add(value);
+		if (f(false, true)) {
+			for (const value of B) {
+				if (!A.has(value)) {
+					result.add(value);
+				}
 			}
 		}
 		return result;
